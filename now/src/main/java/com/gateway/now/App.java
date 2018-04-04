f3eea672d80a3dc7c246282fcc8a15940e08ccb4
wudongping <dongping.wu@comgateway.com>
2018-04-04 16:58:49 +0800

    initial commit

---
+++ now/src/main/java/com/gateway/now/App.java
@@ -0,0 +1,13 @@
+package com.gateway.now;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
